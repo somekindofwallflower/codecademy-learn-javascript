840dec10131a9083739c0cbf6abe76710bd014f8
somekindofwallflower <someone.ana13@gmail.com>
2022-10-27 21:39:53 +0200

    Did 03-Conditionals / 1-Conditional-Statements / 07-Truthy-and-Falsy-Assignment

---
+++ 03-Conditionals/1-Conditional-Statements/07-Truthy-and-Falsy-Assignment/main.js
@@ -0,0 +1,6 @@
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
