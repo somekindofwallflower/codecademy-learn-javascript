e685b7bb6d01ccd6f4336f52e3ed9b443f16b7f0
somekindofwallflower <someone.ana13@gmail.com>
2022-10-28 19:08:13 +0200

    Did 06-Arrays / 1-Arrays / 05-Arrays-with-let-and-const

---
+++ 06-Arrays/1-Arrays/05-Arrays-with-let-and-const/main.js
@@ -0,0 +1,7 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+condiments = new Array['Mayo'];
