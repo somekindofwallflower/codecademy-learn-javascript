5ceff7d0763786978ba91ffe82f1b6d76093ae1f
somekindofwallflower <someone.ana13@gmail.com>
2022-10-28 19:32:29 +0200

    Did 07-Loops / 1-Loops / 09-The-Break-Keyword

---
+++ 07-Loops/1-Loops/09-The-Break-Keyword/main.js
@@ -0,0 +1,10 @@
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+for (let i = 0; i < rapperArray.length; i++) {
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === 'Notorious B.I.G.') {
+    break;
+  }
+}
+
+console.log("And if you don't know, now you know.");
