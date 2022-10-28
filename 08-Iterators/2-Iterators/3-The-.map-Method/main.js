f4ea1b5927acfdcae3eda49750a14682ce121b45
somekindofwallflower <someone.ana13@gmail.com>
2022-10-28 20:40:34 +0200

    Did 08-Iterators / 2-Iterators / 3-The-.map-Method

---
+++ 08-Iterators/2-Iterators/3-The-.map-Method/main.js
@@ -0,0 +1,12 @@
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => animal[0]);
+console.log(secretMessage);
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(bigNumber => bigNumber / 100);
+console.log(smallNumbers);
