cd06b67767ab04d003e0e5c6cdf82cf7a1d0c740
somekindofwallflower <someone.ana13@gmail.com>
2022-10-28 20:23:20 +0200

    Did 09-Objects / 1-Objects / 03-Accessing-Properties

---
+++ 09-Objects/1-Objects/03-Accessing-Properties/app.js
@@ -0,0 +1,13 @@
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    flightPath: 'Milky Way',
+    numCrew: 5
+  };
+  
+  // Write your code below
+  const crewCount = spaceship.numCrew;
+  console.log(crewCount);
+  const planetArray = spaceship.flightPath;
+  console.log(planetArray);
+  
