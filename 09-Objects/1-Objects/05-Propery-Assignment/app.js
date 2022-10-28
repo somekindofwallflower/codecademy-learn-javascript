239c086cb7c9d330dd0b4d834a0b7345ea4b5c53
somekindofwallflower <someone.ana13@gmail.com>
2022-10-28 20:24:07 +0200

    Did 09-Objects / 1-Objects / 05-Propery-Assignment

---
+++ 09-Objects/1-Objects/05-Propery-Assignment/app.js
@@ -0,0 +1,12 @@
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+  };
+  
+  // Write your code below
+  spaceship.color = 'glorious gold';
+  spaceship.numEngines = 9;
+  delete spaceship['Secret Mission'];
+  console.log(spaceship);
