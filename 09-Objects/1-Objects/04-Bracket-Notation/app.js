196762d29f3d46d974b5d715f43ce51ad250e9e6
somekindofwallflower <someone.ana13@gmail.com>
2022-10-28 20:23:33 +0200

    Did 09-Objects / 1-Objects / 04-Bracket-Notation

---
+++ 09-Objects/1-Objects/04-Bracket-Notation/app.js
@@ -0,0 +1,14 @@
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    'Active Mission' : true,
+    homePlanet : 'Earth', 
+    numCrew: 5
+   };
+  
+  let propName = 'Active Mission';
+  
+  // Write your code below
+  let isActive = spaceship['Active Mission'];
+  console.log(spaceship[propName]);
+  console.log(isActive);
+  
